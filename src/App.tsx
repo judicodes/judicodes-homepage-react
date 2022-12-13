3f20e20a5e9ac84c886459b3f8e011ccfbdc14a6
Judith <72439973+judicodes@users.noreply.github.com>
2022-12-13 18:22:46 +0100

    Set up Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { ChakraProvider, Box } from '@chakra-ui/react';
 
 function App() {
-  return <h1>judicodes v2 under construction...</h1>;
+  return (
+    <ChakraProvider>
+      <Box bg='teal.100' height='full'>
+        <h1>judicodes v2 under construction...</h1>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
